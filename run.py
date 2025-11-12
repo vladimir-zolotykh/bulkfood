7fc5198a1d73f351de0b6121499e225440ad7fc1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-12 13:02:09 +0200

    Implement base code
    Add classes: Descriptor, Validated, Quantity, NonBlank, EntityMeta,
    Entity, LineItem

--- run.py
+++ run.py
@@ -1,7 +1,73 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from abc import ABC, abstractmethod
+
+
+class Descriptor:
+    def __set_name__(self, owner, name):
+        self.storage_name = f"{self.__class__.__name__}#{name}"
+
+    def __get__(self, instance, owner):
+        if not instance:
+            return self
+        return getattr(instance, self.storage_name)
+
+    def __set__(self, instance, value):
+        value = self.validate(instance, value)
+        setattr(instance, self.storage_name, value)
+
+
+class Validated(ABC, Descriptor):
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class Quantity(Validated):
+    def validate(self, value):
+        if value < 0:
+            raise ValueError(f"{value} must be > 0")
+        return value
+
+
+class NonBlank(Validated):
+    def validate(self, value):
+        value = value.strip()
+        if not value:
+            raise ValueError(f"{value} str must be None")
+        return value
+
+
+class EntityMeta(type):
+    def __init__(cls, clsname, bases, clsdict):
+        super().__init__(clsname, bases, clsdict)
+        fields = []
+        for name, value in clsdict.items():
+            if isinstance(value, Descriptor):
+                descriptor = value
+                descriptor.name = name
+                fields.append(name)
+        setattr(cls, "_field_names", fields)
+
+
+class Entity(metaclass=EntityMeta):
+    @classmethod
+    def iter_fields(cls):
+        for name in cls._field_names:
+            yield name
+
+
+class LineItem(Entity):
+    description = NonBlank()
+    weight = Quantity()
+    price = Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
 
 
 if __name__ == "__main__":
-    pass
+    raisins = LineItem("Golden raisins", 10, 6.95)
